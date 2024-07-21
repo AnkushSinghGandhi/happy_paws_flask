e9ee75f6b087d6ecd543590c9ccbc8b35d410378
Ankush Singh Gandhi <ankushsinghgandhi@gmail.com>
2024-07-21 18:36:57 +0530

    user controller added

---
+++ controllers/user_controller.py
@@ -0,0 +1,27 @@
+from flask import request, jsonify
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import create_access_token
+from models import User, db
+
+def create_user():
+    data = request.get_json()
+
+    if User.query.filter_by(username=data['username']).first():
+        return jsonify({'message': 'User already exists'}), 400
+
+    hashed_password = generate_password_hash(data['password'])
+    new_user = User(username=data['username'], password=hashed_password, role=data['role'])
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({'message': 'User created successfully'}), 201
+
+def login_user():
+    data = request.get_json()
+    user = User.query.filter_by(username=data['username']).first()
+
+    if not user or not check_password_hash(user.password, data['password']):
+        return jsonify({'message': 'Invalid credentials'}), 401
+
+    access_token = create_access_token(identity=user.id)
+    return jsonify({'access_token': access_token}), 200
